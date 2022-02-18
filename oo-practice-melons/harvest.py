58f4d9be8f16b02c108c89560ab0bbc25edc5d54
ckchalmers <50804745+ckchalmers@users.noreply.github.com>
2022-02-18 04:33:04 +0000

    more melontypes

--- oo-practice-melons/harvest.py
+++ oo-practice-melons/harvest.py
@@ -12,18 +12,20 @@ class MelonType:
         """Initialize a melon."""
 
         self.pairings = []
-
-        # Fill in the rest
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
-
-        # Fill in the rest
+        self.pairings.append(pairing)
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
-
-        # Fill in the rest
+        self.code = new_code
 
 
 def make_melon_types():
@@ -31,15 +33,60 @@ def make_melon_types():
 
     all_melon_types = []
 
-    # Fill in the rest
+    musk = MelonType( 
+        "musk",
+        "Muskmelon",
+         1998,
+        "green",    
+        True,
+        True
+        )
+    musk.add_pairing("mint")
+    all_melon_types.append("musk")
+
+
+    cas = MelonType(
+        "cas",
+        "Casaba",
+        2003,
+        "orange",
+        False,
+        False
+    )
+    cas.add_pairing(["strawberries", "mint"]) 
+    all_melon_types.append("cas")
+    
+
+    cren = MelonType(
+        "cren",
+        "Crenshaw",
+        1996,
+        "green",
+        False,
+        False
+    )
+    cren.add_pairing("prosciutto")
+    all_melon_types.append("cren")
+
+
+    yw = MelonType(
+        "yw",
+        "Yellow Watermelon",
+        2013,
+        "yellow",
+        False,
+        True
+    )
+    yw.add_pairing("ice cream")
+    all_melon_types.append("yw")
 
     return all_melon_types
 
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
-
-    # Fill in the rest
+    for melon in melon_types:
+        print()
 
 
 def make_melon_type_lookup(melon_types):
