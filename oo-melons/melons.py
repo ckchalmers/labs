bc3cdf7ee8e1cf3d67857141f42bed7654c03b9e
ckchalmers <50804745+ckchalmers@users.noreply.github.com>
2022-02-26 23:41:47 +0000

    initial commit

--- oo-melons/melons.py
+++ oo-melons/melons.py
@@ -1,23 +1,35 @@
+from random import randint
+from datetime import date, time
+
 """Classes for melon orders."""
 class AbstractMelonOrder:
     """An abstract base class that other Melon Orders inherit from."""
-    #species = none
-    #qty = 0
-    #shipped = False
+    
     order_type = None
     tax = 0
-
+ 
     def __init__(self, species, qty):
        """Initialize melon order attributes."""
        
        self.species = species
        self.qty = qty
-       self.shipped = False
-   
+       if qty > 100:
+           raise TooManyMelonsError()
+       self.shipped = False    
+    
+    @staticmethod
+    def get_base_price():
+        """set a random base price between 5 and 9"""
+        base_price = randint(5,9)
+        if date.weekday(date.today()) in range(0,5):
+                if time.hour() in range(8,12):
+                    base_price += 4
+        return base_price
+        
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        base_price = self.get_base_price()
         total = (1 + self.tax) * self.qty * base_price
         if self.species == "Christmas melon":
             total *= 1.5
@@ -75,3 +87,10 @@ class GovernmentMelonOrder(AbstractMelonOrder):
         """Marks inspections as 'passed' """
         if passed:
             self.passed_inspection = True
+
+class TooManyMelonsError(Exception):
+    """Raised when someone tries to order more than 100 melons"""
+    def __init__(self):
+        #self.message = message
+        super().__init__("Too many melons!")
+        #self.message="Too many melons!"
